7fe73a0974b615c860a889bc771950bbb8ed290c
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-12 22:10:07 +0200

    Create a project

---
+++ Podcasts/Podcasts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podcasts
+//
+//  Created by Ghaiath Alhereh on 12.07.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
