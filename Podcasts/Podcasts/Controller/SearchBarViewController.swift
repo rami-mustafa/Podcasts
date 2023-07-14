5d498a3d6c5e722dd116b35fb8f3a20ce6379c27
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-14 16:00:26 +0200

    Search Bar

--- Podcasts/Podcasts/Controller/SearchBarViewController.swift
+++ Podcasts/Podcasts/Controller/SearchBarViewController.swift
@@ -12,6 +12,11 @@ private let reuseIdentifier = "SearchCell"
 class SearchBarViewController: UITableViewController {
     
      // MARK: - Properties
+    
+    var searchResult: [Podcast] = []{
+           didSet{ tableView.reloadData() }
+       }
+    
      // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,28 +50,47 @@ extension SearchBarViewController{
 extension SearchBarViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return searchResult.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! SearchBarCell
- 
+        cell.result = self.searchResult[indexPath.row]
         return cell
     }
     
 }
 
+// MARK: - UITableViewDelegate
+extension SearchBarViewController {
+   override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+       let label = UILabel()
+       label.text = "Start Searching All Podcasts.."
+       label.textAlignment = .center
+       label.font = UIFont.preferredFont(forTextStyle: .title1)
+       label.textColor = .systemPurple
+       return label
+   }
+   override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+       return self.searchResult.count == 0 ? 80 : 0
+   }
+
+}
+
+
 
 // MARK: - UISearchBarDelegate
 extension  SearchBarViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        
-        AF.request("https://itunes.apple.com/search?term=jack+johnson&limit=25.").responseData { response in
-            print(String(data: response.data!, encoding: .utf8))
+        SearchService.fetchData(searchText: searchText) { result in
+            self.searchResult = result
+            
         }
-       
-        
     }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+          self.searchResult = []
+      }
 }
 
 
