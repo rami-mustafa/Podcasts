a635a1b203d0fb1b822e6782ce906546f14f7c46
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-15 17:44:55 +0200

    Episode Servie

---
+++ Podcasts/Podcasts/Service/EpisodeService.swift
@@ -0,0 +1,21 @@
+//
+//  EpisodeServie.swift
+//  Podcasts
+//
+//  Created by Ghaiath Alhereh on 14.07.23.
+//
+
+import Foundation
+import FeedKit
+
+struct EpisodeServie {
+    
+    static func fetchData(urlString: String){
+        
+        let feedKit = FeedParser(URL: URL(string: urlString )!)
+        
+        }
+        
+}
+    
+
