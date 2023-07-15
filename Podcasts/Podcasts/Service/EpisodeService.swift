3abaebc905c4a49080fe304232daef6e1dd9f719
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-16 00:19:09 +0200

    Episode Servcie

--- Podcasts/Podcasts/Service/EpisodeService.swift
+++ Podcasts/Podcasts/Service/EpisodeService.swift
@@ -8,13 +8,34 @@
 import Foundation
 import FeedKit
 
-struct EpisodeServie {
+struct EpisodeServcie {
     
-    static func fetchData(urlString: String){
+    static func fetchData(urlString: String , completion: @escaping([Episode]) -> Void ){
+        var episodeResult: [Episode] = []
         
         let feedKit = FeedParser(URL: URL(string: urlString )!)
-        
+        feedKit.parseAsync { result in
+            switch result {
+                
+            case .success(let feed):
+                switch feed{
+                    
+                case .rss(let feedResult):
+                    feedResult.items?.forEach({ value in
+                 let episodeCell = Episode(feedItem: value)
+                        episodeResult.append(episodeCell)
+                        completion(episodeResult)
+                    })
+                case .json(_):
+                    break
+                case .atom(_):
+                    break
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
         }
+    }
         
 }
     
