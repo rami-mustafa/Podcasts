0ac002c164563aea0c1e9b5113b8c4a2b77dc76e
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-16 00:19:25 +0200

    Episode Cell

---
+++ Podcasts/Podcasts/View/EpisodeCell.swift
@@ -0,0 +1,108 @@
+//
+//  EpisodeCell.swift
+//  Podcasts
+//
+//  Created by Ghaiath Alhereh on 14.07.23.
+//
+
+import UIKit
+import Kingfisher
+
+
+class EpisodeCell: UITableViewCell {
+    //    MARK: - prooerties
+    var episodeResult: Episode?{
+        didSet{
+            configure()
+        }
+    }
+    private let episodeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.customMode()
+        imageView.backgroundColor = .systemPink
+        imageView.layer.cornerRadius = 12
+        return imageView
+    }()
+    
+    private let pubDateLabel: UILabel = {
+          let label = UILabel()
+           label.textColor = .systemPurple
+           label.font = UIFont.systemFont(ofSize: 16)
+           label.text = "pubDateLabel"
+           return label
+       }()
+       private let titleLabel: UILabel = {
+          let label = UILabel()
+           label.textColor = .black
+           label.font = UIFont.boldSystemFont(ofSize: 18)
+           label.text = "titleLabel"
+           label.numberOfLines = 2
+           return label
+       }()
+       private let decriptionLabel: UILabel = {
+          let label = UILabel()
+           label.textColor = .lightGray
+           label.font = UIFont.systemFont(ofSize: 14)
+           label.numberOfLines = 2
+           label.text = "decriptionLabel"
+           return label
+       }()
+    
+    private var stackView: UIStackView!
+
+    
+    
+    //    MARK: - Lifecyle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Helpers
+extension EpisodeCell {
+    private func setup() {
+        configureUI()
+    }
+    
+    private func configureUI(){
+        
+        episodeImageView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(episodeImageView)
+        NSLayoutConstraint.activate([
+            episodeImageView.heightAnchor.constraint(equalToConstant: 100),
+            episodeImageView.widthAnchor.constraint(equalToConstant: 100),
+            episodeImageView.leadingAnchor.constraint(equalTo: leadingAnchor , constant: 10),
+            episodeImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            
+        ])
+        
+        stackView = UIStackView(arrangedSubviews: [pubDateLabel,titleLabel,decriptionLabel])
+               stackView.translatesAutoresizingMaskIntoConstraints = false
+               stackView.axis = .vertical
+               stackView.spacing = 5
+               addSubview(stackView)
+               NSLayoutConstraint.activate([
+                   stackView.centerYAnchor.constraint(equalTo: episodeImageView.centerYAnchor),
+                   stackView.leadingAnchor.constraint(equalTo: episodeImageView.trailingAnchor, constant: 10),
+                   stackView.trailingAnchor.constraint(equalTo: trailingAnchor,constant: -10)
+        ])
+    }
+    
+    private func configure(){
+        guard let episode = self.episodeResult else { return }
+        
+          let viewModel = EpisodeCellViewModel(episode: episode)
+                self.episodeImageView.kf.setImage(with: viewModel.profileImageUrl)
+                self.titleLabel.text = viewModel.title
+                self.decriptionLabel.text = viewModel.description
+                self.pubDateLabel.text = viewModel.pubDate
+        
+    }
+}
+
