457aff03b99ee45343324302d2c42dd62e803e41
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-16 00:17:59 +0200

    Episode Cell View Model

---
+++ Podcasts/Podcasts/ViewModel/EpisodeCellViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  EpisodecellViewModel.swift
+//  Podcasts
+//
+//  Created by Ghaiath Alhereh on 15.07.23.
+//
+
+import Foundation
+
+struct EpisodeCellViewModel {
+    let episode: Episode!
+    init(episode: Episode!) {
+        self.episode = episode
+    }
+    var profileImageUrl: URL?{
+        return URL(string: episode.imageUrl!)
+    }
+    
+    var title: String?{
+           return episode.title
+       }
+       var description: String?{
+           return episode.description
+       }
+       var pubDate: String?{
+           let dateFormatter = DateFormatter()
+           dateFormatter.dateFormat = "MMM dd, yyy"
+           return dateFormatter.string(from: episode.pubDate)
+       }
+    
+}
