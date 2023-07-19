9ad95441d2291d2305a08f0644a289a5ff7a45ad
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-19 21:02:01 +0200

    Favorite View Controller

--- Podcasts/Podcasts/Controller/FavoriteViewController.swift
+++ Podcasts/Podcasts/Controller/FavoriteViewController.swift
@@ -5,30 +5,51 @@
 //  Created by Ghaiath Alhereh on 13.07.23.
 
 import UIKit
- 
-class FavoriteViewController: UIViewController {
+
+class FavoriteViewController: UICollectionViewController {
     
-     // MARK: - Properties
-     // MARK: - Lifecycle
+    // MARK: - Properties
+    
+    fileprivate let cellId = "cellId"
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
         fetchData()
     }
-        
- }
-
-// MARK: - Helpers
-extension FavoriteViewController{
-    
-    private func fetchData(){
-        
-    }
-    private func setup(){
-        view.backgroundColor = .darkGray
-    }
-    // MARK: - UICollectionViewDataSourc
-    // MARK: - UICollectionViewDelegate
-    // MARK: - UICollectionViewDelegateFlowLayout
     
 }
+
+ // MARK: - Helpers
+extension FavoriteViewController{
+   private func fetchData(){
+       
+   }
+   private func setup(){
+       
+       collectionView.backgroundColor = .white
+
+       collectionView.register(FavoritePodcastCell.self, forCellWithReuseIdentifier: cellId)
+ 
+   }
+}
+// MARK: - UICollectionViewDataSource
+extension FavoriteViewController{
+   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return 5
+   }
+   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! FavoritePodcastCell
+       return cell
+   }
+}
+// MARK: - UICollectionViewDelegate
+extension FavoriteViewController{
+   override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+      
+   }
+}
+
+
+
