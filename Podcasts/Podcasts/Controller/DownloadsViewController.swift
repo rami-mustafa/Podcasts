018c5099589e1770237d8ca2e8d1ca82dd34cb38
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-22 21:39:55 +0200

    DownloadsViewController TableView operations

--- Podcasts/Podcasts/Controller/DownloadsViewController.swift
+++ Podcasts/Podcasts/Controller/DownloadsViewController.swift
@@ -5,32 +5,44 @@
 //  Created by Ghaiath Alhereh on 13.07.23.
 //
 
- import UIKit
-
-class DownloadsViewController: UIViewController {
+import UIKit
+private let reuseIdentifiew = "DownloadCell"
+class DownloadsViewController: UITableViewController {
     
-     // MARK: - Properties
-     // MARK: - Lifecycle
+    // MARK: - Properties
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        fetchData()
     }
-        
- }
+    
+}
 
 // MARK: - Helpers
 extension DownloadsViewController{
     
-    private func fetchData(){
-        
-    }
     private func setup(){
-        view.backgroundColor = .green
-
+        view.backgroundColor = .white
+        tableView.register(EpisodeCell.self, forCellReuseIdentifier: reuseIdentifiew)
     }
-    // MARK: - UICollectionViewDataSourc
-    // MARK: - UICollectionViewDelegate
-    // MARK: - UICollectionViewDelegateFlowLayout
     
-} 
+}
+
+// MARK: - UITableViewDataSource
+extension DownloadsViewController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifiew, for: indexPath)
+        
+        return cell
+    }
+}
+// MARK: - UITableViewDelegate
+extension DownloadsViewController{
+   override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+       return 134
+   }
+ 
+}
