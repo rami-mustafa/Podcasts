d1c6e31c3b448c315330480e55bac9e981e6a512
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-26 22:52:41 +0200

    updata

--- Podcasts/Podcasts/Model/Episode.swift
+++ Podcasts/Podcasts/Model/Episode.swift
@@ -19,11 +19,20 @@ struct Episode: Codable {
     var imageUrl: String?
     
     init(feedItem: RSSFeedItem) {
-        self.streamUrl = feedItem.enclosure?.attributes?.url ?? ""
+        
+        
+        self.author = feedItem.iTunes?.iTunesAuthor?.description ?? feedItem.author ?? ""
         self.title = feedItem.title ?? ""
         self.pubDate = feedItem.pubDate ?? Date()
+        self.streamUrl = feedItem.enclosure?.attributes?.url ?? ""
         self.description = feedItem.iTunes?.iTunesSubtitle ?? feedItem.description ?? ""
-        self.author = feedItem.iTunes?.iTunesAuthor ?? ""
-        self.imageUrl = feedItem.iTunes?.iTunesImage?.attributes?.href ?? ""
+        self.imageUrl = feedItem.iTunes?.iTunesImage?.attributes?.href ?? "https://cdn.pixabay.com/photo/2018/09/23/00/09/podcast-3696504_1280.jpg"
+        //
+        //        self.streamUrl = feedItem.enclosure?.attributes?.url ?? ""
+        //        self.title = feedItem.title ?? ""
+        //        self.pubDate = feedItem.pubDate ?? Date()
+        //        self.description = feedItem.iTunes?.iTunesSubtitle ?? feedItem.description ?? ""
+        //        self.author = feedItem.iTunes?.iTunesAuthor ?? ""
+        //        self.imageUrl = feedItem.iTunes?.iTunesImage?.attributes?.href ?? ""
     }
 }
