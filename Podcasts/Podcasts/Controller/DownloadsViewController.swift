c5a782058548c85384020cb0efb8ad070cd97d9e
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-07-29 22:28:38 +0200

    Making Downloads With Alamofire

--- Podcasts/Podcasts/Controller/DownloadsViewController.swift
+++ Podcasts/Podcasts/Controller/DownloadsViewController.swift
@@ -10,12 +10,20 @@ private let reuseIdentifiew = "DownloadCell"
 class DownloadsViewController: UITableViewController {
     
     // MARK: - Properties
+        private var episodeResult = UserDefaults.downloadEpisodeRead()
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
     }
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.episodeResult = UserDefaults.downloadEpisodeRead()
+        tableView.reloadData()
+        let window = UIApplication.shared.connectedScenes.first as! UIWindowScene
+        let mainTabController = window.keyWindow?.rootViewController as! MainTabBarController
+        mainTabController.viewControllers?[2].tabBarItem.badgeValue = nil
+    }
 }
 
 // MARK: - Helpers
@@ -31,11 +39,11 @@ extension DownloadsViewController{
 // MARK: - UITableViewDataSource
 extension DownloadsViewController{
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return episodeResult.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifiew, for: indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifiew, for: indexPath) as! EpisodeCell
+        cell.episodeResult = episodeResult[indexPath.item]
         return cell
     }
 }
